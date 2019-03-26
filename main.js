6430ebbd42ef9bedb645d666b018be3b81c658dc
Veomany <veomany.khammarath@gmail.com>
2019-03-27 00:17:12 +1100

    changed scroll animation for clicks

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-$("#about-to-top, #project-to-top").on("click", function() {
-  $(window).scrollTop(0);
-},);
+// $("#about-to-top, #project-to-top").on("click", function() {
+//   $(window).scrollTop(0);
+// },);
 
 $(function(){
   $('li').on('click',function(e){
     e.preventDefault();
     $('html, body').animate({
       scrollTop: $($(e.target).attr("href")).offset().top
-    }, 200);
+    }, 500);
+  });
+});
+
+
+$(function(){
+  $('#about-to-top, #project-to-top').on('click',function(e){
+    e.preventDefault();
+    $("html, body").animate({ 
+      scrollTop: 0 
+    }, 500);
   });
 });
